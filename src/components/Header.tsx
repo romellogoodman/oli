697e6a33828f8726173b1753d35c159dfe7dec81
Romello Goodman <romellogood@gmail.com>
2025-11-09 18:58:25 -0500

    Add logo by Chloe Stokes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import Logo from "@/components/Logo";
+
 export default function Header() {
   return (
     <header>
       <div className="header-content">
         <p className="header-logo">
-          <a href="/">OLI</a>
+          <a href="/">
+            <Logo />
+          </a>
         </p>
       </div>
     </header>
